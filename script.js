3d7a4c2ac873b49b00e539cdb80c3424e4b351b7
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-22 04:06:51 -0700

    guess the number done

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //code
 // gets a random number from 1 - 20
 let randomNumber = Math.floor(Math.random() * 20) + 1
+console.log(randomNumber)
+document.querySelector(".check").value*1
+if(input===randomNumber){
+    documnet.querySelector("body").style.backgroundColor = "green"
+    document.querySelector(".message").innerText= "correct Number!"
+    documet.querySelector(".number").innerText= randomNumber
+}
 let score = 20
 let highScore = 0
